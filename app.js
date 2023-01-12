f785b13d030e16fade9d52926674e00af89110f2
İrem <101325423+iremkran@users.noreply.github.com>
2023-01-12 20:03:45 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const container= document.querySelector('.container')
+const unsplashURL= 'https://source.unsplash.com/random/'
+
+const rows = 5
+
+for(let i=0;i<rows * 3;i++){
+    const img=document.createElement('img')
+    img.src=`${unsplashURL}${getResimBoyut()}`
+    container.appendChild(img)
+
+}
+function getResimNo(){
+    return Math.floor(Math.random() * 10 ) + 300
+}
+
+function getResimBoyut(){
+    return`${getResimNo()}x${getResimNo()}`
+}
